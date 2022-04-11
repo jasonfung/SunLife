d8f1dcd4523581101964ee7c17ceb905f4df1718
Jason <jasonka.fung@gmail.com>
2022-04-10 22:55:52 -0400

    Final Solution

--- force-app/main/default/lwc/accountDataTableJason/accountDataTableJason.js
+++ force-app/main/default/lwc/accountDataTableJason/accountDataTableJason.js
@@ -1,74 +1,121 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement } from 'lwc';
 import findAccounts from '@salesforce/apex/sunlifeAccountController.findAccounts';
+import { NavigationMixin } from 'lightning/navigation';
 
-const columns = [
+const actions = [
+    { label: 'View', name: 'view' },
+    { label: 'Edit', name: 'edit' },
+];
+ 
+const columns = [   
     { label: 'Account Name', fieldName: 'AccountURL', type: 'url', typeAttributes: {label: {fieldName: 'Name'}, target: '_blank'}, sortable: true },
     { label: 'Account Owner', fieldName: 'OwnerName', sortable: true },
     { label: 'Phone', fieldName: 'Phone', type: 'phone' },
     { label: 'Website', fieldName: 'Website', type: 'url' },
-    { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' }
+    { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions },
+    },
 ];
 
-/** The delay used when debouncing event handlers before invoking Apex. */
-const DELAY = 300;
-
-export default class accountDataTableJason extends LightningElement {
-    searchKey = '';
-    columns = columns;
+export default class accountDataTableJason extends NavigationMixin( LightningElement ) {
     accounts;
     error;
+    columns = columns;
+    sortedBy;
     defaultSortDirection = 'asc';
     sortDirection = 'asc';
-    sortedBy;
-
-    @wire(findAccounts, { searchKey: '$searchKey' })
-    wiredAccounts({data, error}){
-		if(data) {
-			this.accounts = data.map(row=>{
-                return{...row, OwnerName: row.Owner.Name, Name: row.Name, AccountURL: '/' + row.Id}
+ 
+    connectedCallback(){
+        const searchKey = '';
+        findAccounts( { searchKey } )   
+            .then(result => {
+                this.accounts = result.map(row=>{
+                    return{...row, OwnerName: row.Owner.Name, Name: row.Name, AccountURL: '/' + row.Id}
+                }) 
             })
-			this.error = undefined;
-		}else {
-			this.accounts =undefined;
-			this.error = error;
-		}
-	}
+            .catch(error => {
+ 
+                this.error = error;
+ 
+            });
+    } 
 
     handleKeyChange(event) {
-        // Debouncing this method: Do not update the reactive property as long as this function is
-        // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
-        window.clearTimeout(this.delayTimeout);
+         
         const searchKey = event.target.value;
-        // eslint-disable-next-line @lwc/lwc/no-async-operation
-        this.delayTimeout = setTimeout(() => {
-            this.searchKey = searchKey;
-        }, DELAY);
+        
+        findAccounts( { searchKey } )   
+            .then(result => {
+                this.accounts = result.map(row=>{
+                    return{...row, OwnerName: row.Owner.Name, Name: row.Name, AccountURL: '/' + row.Id}
+                }) 
+            })
+            .catch(error => {
+ 
+                this.error = error;
+ 
+            }); 
     }
 
-    // Used to sort the 'Age' column
-    sortBy(field, reverse, primer) {
-        const key = primer
-            ? function(x) {
-                return primer(x[field]);
-            }
-            : function(x) {
-                return x[field];
-            };
+    handleRowAction( event ) {
 
-        return function(a, b) {
-            a = key(a);
-            b = key(b);
-            return reverse * ((a > b) - (b > a));
-        };
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        switch ( actionName ) {
+            case 'view':
+                this[NavigationMixin.Navigate]({
+                    type: 'standard__recordPage',
+                    attributes: {
+                        recordId: row.Id,
+                        actionName: 'view'
+                    }
+                });
+                break;
+            case 'edit':
+                this[NavigationMixin.Navigate]({
+                    type: 'standard__recordPage',
+                    attributes: {
+                        recordId: row.Id,
+                        objectApiName: 'Account',
+                        actionName: 'edit'
+                    }
+                });
+                break;
+            default:
+        }
+
     }
+    
+    onHandleSort( event ) {
 
-    onHandleSort(event) {
         const { fieldName: sortedBy, sortDirection } = event.detail;
-        const cloneData = [...this.data];
+        const cloneData = [...this.accounts];
 
-        cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
-        this.data = cloneData;
+        cloneData.sort( this.sortBy( sortedBy, sortDirection === 'asc' ? 1 : -1 ) );
+        this.accounts = cloneData;
         this.sortDirection = sortDirection;
         this.sortedBy = sortedBy;
+
     }
+
+    sortBy( field, reverse, primer ) {
+
+        const key = primer
+            ? function( x ) {
+                  return primer(x[field]);
+              }
+            : function( x ) {
+                  return x[field];
+              };
+
+        return function( a, b ) {
+            a = key(a);
+            b = key(b);
+            return reverse * ( ( a > b ) - ( b > a ) );
+        };
+
+    }
+
 }
